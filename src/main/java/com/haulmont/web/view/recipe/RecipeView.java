2fd003f264ff3719ff8ef4af4791eda4662ff64f
lovk4ch <lovk4ch@gmail.com>
2019-11-18 08:06:30 +0300

    Change add/update logic for recipes

--- src/main/java/com/haulmont/web/view/recipe/RecipeView.java
+++ src/main/java/com/haulmont/web/view/recipe/RecipeView.java
@@ -11,8 +11,6 @@ import java.util.stream.Collectors;
 
 public class RecipeView extends VerticalLayout {
 
-    private RecipeEdit recipeEdit = new RecipeEdit(this);
-
     private TextField filterPatient, filterDescription;
     private ComboBox filterPriority;
 
@@ -52,10 +50,14 @@ public class RecipeView extends VerticalLayout {
         addComponents(grid);
 
         addButton = new Button(Consts.ADD,
-                event -> recipeEdit.add(new RecipeRow()));
+                event -> {
+                    RecipeEdit recipeEdit = new RecipeEdit(this);
+                    recipeEdit.add(new RecipeRow());
+                });
 
         updateButton = new Button(Consts.UPDATE,
                 event -> {
+                    RecipeEdit recipeEdit = new RecipeEdit(this);
                     if (grid.asSingleSelect().getValue() != null) {
                         recipeEdit.edit(grid.asSingleSelect().getValue());
                     } else {
